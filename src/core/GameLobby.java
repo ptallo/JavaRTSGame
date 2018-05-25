9de6433570ccf48ca961607f1aa62b11a1a3a231
Philip Tallo <tallopt@mail.uc.edu>
2018-05-24 21:17:21 -0400

    made GameLobby and Player implement Serializable

--- src/core/GameLobby.java
+++ src/core/GameLobby.java
@@ -1,15 +1,22 @@
 package core;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.UUID;
 
-public class GameLobby {
+public class GameLobby implements Serializable {
     private String id;
-    private ArrayList<Player> players = new ArrayList<>();
+    private String lobbyName;
     private Player lobbyOwner;
+    private ArrayList<Player> players = new ArrayList<>();
 
-    public GameLobby(Player owner) {
+    public GameLobby(Player owner, String name) {
         id = UUID.randomUUID().toString();
         lobbyOwner = owner;
+        lobbyName = name;
+    }
+
+    public void addPlayer(Player player){
+        players.add(player);
     }
 }
--- src/core/Player.java
+++ src/core/Player.java
@@ -1,8 +1,9 @@
 package core;
 
+import java.io.Serializable;
 import java.util.UUID;
 
-public class Player {
+public class Player implements Serializable {
     private String id;
 
     public Player() {
