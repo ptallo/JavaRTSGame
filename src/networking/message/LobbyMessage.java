9ef90e35030951550e3d4c3b19862ea0ded8cdce
Philip Tallo <tallopt@mail.uc.edu>
2018-05-30 19:11:42 -0400

    removed messages classes will use a indicator

--- src/networking/message/LobbyMessage.java
+++
@@ -1,9 +0,0 @@
-package networking.message;
-
-import core.GameLobby;
-
-public class LobbyMessage extends GameMessage<GameLobby> {
-    public LobbyMessage(String message, GameLobby object) {
-        super(message, object);
-    }
-}
